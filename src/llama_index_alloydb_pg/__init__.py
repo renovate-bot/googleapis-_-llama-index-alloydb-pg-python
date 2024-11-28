1c73e72d1cec81e6e477c39ee39f83d6e967c7f1
dishaprakash <57954147+dishaprakash@users.noreply.github.com>
2024-11-28 11:24:10 +0000

    feat: Adding AlloyDBIndexStore (#13)
    * feat: Adding AlloyDBIndexStore
    
    * Linter fix.
    
    * Fix tests.
    
    * Fix Linter
    
    * Added docstrings
    
    * Minor change in docstring

--- src/llama_index_alloydb_pg/__init__.py
+++ src/llama_index_alloydb_pg/__init__.py
@@ -14,5 +14,6 @@
 
 from .document_store import AlloyDBDocumentStore
 from .engine import AlloyDBEngine, Column
+from .index_store import AlloyDBIndexStore
 
-_all = ["AlloyDBDocumentStore", "AlloyDBEngine", "Column"]
+_all = ["AlloyDBDocumentStore", "AlloyDBEngine", "AlloyDBIndexStore", "Column"]
