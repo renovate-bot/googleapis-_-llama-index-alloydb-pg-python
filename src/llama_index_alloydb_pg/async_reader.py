98f6c65fc77cbb7f25b22d7118bbb89f3c674b2f
dishaprakash <57954147+dishaprakash@users.noreply.github.com>
2025-01-25 00:55:44 +0000

    fix: Update lazy_load_data return type to Iterable. (#61)
    Co-authored-by: Averi Kitsch <akitsch@google.com>

--- src/llama_index_alloydb_pg/async_reader.py
+++ src/llama_index_alloydb_pg/async_reader.py
@@ -259,7 +259,7 @@ class AsyncAlloyDBReader(BasePydanticReader):
                     self._metadata_json_column,
                 )
 
-    def lazy_load_data(self) -> Iterator[Document]:
+    def lazy_load_data(self) -> Iterable[Document]:
         raise NotImplementedError(
             "Sync methods are not implemented for AsyncAlloyDBReader. Use AlloyDBReader interface instead."
         )
